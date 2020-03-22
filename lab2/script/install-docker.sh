978b7d594557e05629eba468b83b88b73f7e7ecf
jdtotow <ttjd2005@yahoo.fr>
2020-03-22 11:40:59 +0200

    install docker script

---
+++ lab2/script/install-docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update 
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt-get update 
+sudo apt install docker-ce
+
