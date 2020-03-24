fed7eb372582fa7b03bdddee63a17ed344f58ae1
csymvoul <csymvoul@DESKTOP-4HT3TL7.localdomain>
2020-03-24 14:03:53 +0200

    added logical operators example

---
+++ lab1/if_for/if_1.py
@@ -0,0 +1,9 @@
+a = 10
+b = 15
+
+if a > b:
+    print(a , ' is bigger than ' , b)
+elif b > a: 
+    print(b , ' is bigger than ' , a)
+else: 
+    print("they are equal")
---
+++ lab1/if_for/logical_op.py
@@ -0,0 +1,15 @@
+a = "this is a string"
+b = "this"
+
+if b not in a:
+    print("b is not in a")
+else: 
+    print("b is in a")
+
+a = "this is a string"
+b = "hello"
+
+if b not in a:
+    print("b is not in a")
+else: 
+    print("b is in a")
