cdc19f2300c599d758bac9424d5ab649cff893b7
Chrysostomos Symvoulidis <simvoul@unipi.gr>
2020-04-17 15:32:13 +0300

    accepted changes

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -5,11 +5,6 @@ def main():
         print({'msg': 'Info log','time':time.time()})
         time.sleep(2)
 
-<<<<<<< HEAD
 
 if __name__ == "__main__":
     main()
-=======
-if __name__=="__main__":
-    main()
->>>>>>> aef302b207ffd515094192f239229c20e97d62f5
