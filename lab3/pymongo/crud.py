c3ad63ba9fde78e8abbecac45adf59638003e72c
Chrysostomos Symvoulidis <simvoul@unipi.gr>
2020-04-18 10:21:43 +0300

    added basic crud on lab3/pymongo

--- lab3/pymongo/crud.py
+++ lab3/pymongo/crud.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import json
+from pprint import pprint
 
 # Connect to our local MongoDB
 client = MongoClient('mongodb://localhost:27017/')
@@ -21,16 +22,20 @@ def insert_student():
         "gender": "male"
     }
     res = students.insert_one(student)
+    print(res.inserted_id)
 
 def get_all_students():
+    # Find all students
     iterable = students.find()
     for student in iterable:
         print(student)
+
+    # Print total count of students in the Students collection
     print("Total number of students: ", iterable.count())
 
 def find_student(name):
     student = students.find_one({"name": name},
-                                {"_id":0, "name": 1, "email":1})
+                                {"_id":0})
     print(student)
 
 def update_student():
@@ -43,20 +48,27 @@ def update_student():
             "postcode": 18532
         }]
     }'''
-    obj = json.loads(student)
-    print(type(obj))
-    
-    # students.update_one({"email":"Symvoulidis"}, 
-    #                     {
-    #                         "$set":{
-    #                             "email": obj["email"], 
-    #                             "yearOfBirth": student["yearOfBirth"] #,
-    #                             # "address.city": student["address][0]["city"]
-    #                         }
-    #                     })
 
-update_student()
+    student = json.loads(student)
+    res = students.update_many({"email":"Symvoulidis"}, 
+                        {
+                            "$set":{
+                                "email": student["email"], 
+                                "yearOfBirth": student["yearOfBirth"], 
+                                "address.0.city": student["address"][0]["city"],
+                                "address.0.street": student["address"][0]["street"], 
+                                "address.0.postcode": student["address"][0]["postcode"]
+                            }
+                        })
+    print("Totally modified ", res.modified_count, " student")
+    find_student("Chrysostomos Symvoulidis")
 
 def delete_student():
-    res = students.find_one_and_delete({"email": "Symvoulidis"})
-    print(res)
+    res = students.find_one_and_delete({"email": "simvoul@unipi.gr"})
+    print("Deleted the student: ", res["_id"])
+
+# insert_student()
+# update_student()
+# get_all_students()
+# find_student(name="Chrysostomos Symvoulidis")
+delete_student()
